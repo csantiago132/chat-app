2a7392fc240223e5b092922d0aebb588d8e8a92a
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-29 20:13:13 -0500

    LoginComponent

---
+++ src/components/LoginComponent/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import LoginComponent from '../index';
+
+describe('<LoginComponent />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

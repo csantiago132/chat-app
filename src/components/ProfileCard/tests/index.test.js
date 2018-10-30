4189f8b74abf8fb8520ffb443ec7a0eb0ff5846e
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-29 20:15:30 -0500

    ProfileCard

---
+++ src/components/ProfileCard/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ProfileCard from '../index';
+
+describe('<ProfileCard />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

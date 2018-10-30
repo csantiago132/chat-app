fee1ed63a3a6d0720c27c7775a59f6af3480ed2f
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-29 20:12:44 -0500

    LoadingIndicator

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const LoadingIndicator = () => <section>Loading...</section>;
+
+export default LoadingIndicator;
---
+++ src/components/LoadingIndicator/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingIndicator = () => (
+  <section>
+    Loading...
+  </section>
+);
+
+export default LoadingIndicator;

c78d35b96c7f3e0c38399c631c777c9a6424778c
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-11-07 19:28:15 -0600

    adding script to prettier-style all js files

--- doczrc.js
+++ doczrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  title: 'Slack Chat',
-  wrapper: '../../src/setup/docz_wrapper/wrapper',
+  title: "Slack Chat",
+  wrapper: "../../src/setup/docz_wrapper/wrapper",
   typescript: true,
   codeSandbox: false,
   themeConfig: {
-    mode: 'dark',
-    radii: '4px',
+    mode: "dark",
+    radii: "4px",
     styles: {
       container: {
-        width: [1200, '100%'],
-        padding: ['20px', '0 40px 40px'],
+        width: [1200, "100%"],
+        padding: ["20px", "0 40px 40px"],
       }, 
     table: { 
         overflowY: "hidden",
-        overflowX: ['initial', 'initial', 'initial', 'hidden'],
-        display: ['block', 'block', 'block', 'table'],
-        width: '100%',
+        overflowX: ["initial", "initial", "initial", "hidden"],
+        display: ["block", "block", "block", "table"],
+        width: "100%",
         marginBottom: [20, 40],
-        fontFamily: '"Source Code Pro", monospace',
+        fontFamily: ""Source Code Pro", monospace",
         fontSize: 14,
       },
     }

9b067a3cac8d9f1ebd0c1c398786d6f5b929866c
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-11-12 10:30:46 -0600

    updating documentation

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -8,13 +8,7 @@ import db from "./db.json";
 import Wrapper from "../../src/setup/docz_wrapper/wrapper";
 
 const Root = () => (
-  <Theme
-    db={db}
-    imports={imports}
-    hashRouter={false}
-    websocketUrl="ws://127.0.0.1:60505"
-    wrapper={Wrapper}
-  />
+  <Theme db={db} imports={imports} hashRouter={false} wrapper={Wrapper} />
 );
 
 // TODO: this is temporary until react-hot-loader fix hooks issues
